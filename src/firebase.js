47300511f61a57a6adf44642f490c8d54659c724
yearofthefinch <yearofthefinch@gmail.com>
2020-12-26 14:36:49 -0600

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBoQMmHdmYP_dsi2B0jCFoXRHKjbfdIewk',
+  authDomain: 'u-freight-ease.firebaseapp.com',
+  projectId: 'u-freight-ease',
+  storageBucket: 'u-freight-ease.appspot.com',
+  messagingSenderId: '908361200170',
+  appId: '1:908361200170:web:ca413140c0835207f74953',
+  measurementId: 'G-SB9DE5EX55',
+};
+
+export default firebaseConfig;
