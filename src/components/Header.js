09d3872c55e44547f5df0c284b9b3b2132122e40
yearofthefinch <yearofthefinch@gmail.com>
2020-12-29 20:59:17 -0600

    added borderDiv

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,12 +11,16 @@ function Header() {
     <div className="header">
       {/* Header Left */}
       <div className="header__left">
-        <div className="header__logo">u-FREIGHT-ease</div>
-        <div className="header__sendTo">
-          <LocationOnIcon className="header__location" />
-          <div className="header__address">
-            <span className="header__optionLineOne">Deliver to GUEST</span>
-            <span className="header__optionLineTwo">City ZIPCODE</span>
+        <div className="header__borderDiv">
+          <div className="header__logo">u-FREIGHT-ease</div>
+        </div>
+        <div className="header__borderDiv">
+          <div className="header__sendTo">
+            <LocationOnIcon className="header__location" />
+            <div className="header__address">
+              <span className="header__optionLineOne">Deliver to GUEST</span>
+              <span className="header__optionLineTwo">City ZIPCODE</span>
+            </div>
           </div>
         </div>
       </div>
@@ -37,24 +41,32 @@ function Header() {
 
       {/* Header Right */}
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineTwo">Prime</span>
+        <div className="header__borderDiv">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
         </div>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello, Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
+        <div className="header__borderDiv">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello, Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
         </div>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
+        <div className="header__borderDiv">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
         </div>
 
-        <div className="header__optionBasket">
-          <ShoppingBasket />
-          <span className="header__optionLineTwo header__basketCount">0</span>
+        <div className="header__borderDiv">
+          <div className="header__optionBasket">
+            <ShoppingBasket />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
         </div>
       </div>
     </div>
